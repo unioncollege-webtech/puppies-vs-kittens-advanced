d8a28cad335fd1e9a075433894b35cbdbf1e6f79
Caleb <cawright@ucollege.edu>
2015-11-19 02:12:20 +0000

    finished advanced assignment

--- server.js
+++ server.js
@@ -3,89 +3,49 @@ var express = require('express');
 var hbs = require("hbs");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
+var Count = require("./counter.js");
 
 // Create a new express application instance by calling `express()`
 var app = express();
 
 // Serve files in the 'public' directory with Express's built-in static file server
 app.use(express.static('public'));
+
 app.set('view engine', 'hbs');
-app.use(bodyParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(cookieParser());
 // Create a Counter class that will be used to create counter objects
 // See the full description in README.md
 // .
 
-function Count(){
-    this.haveVotes = {
-        puppies: 0,
-        kittens: 0
-    };
-}
-
-Count.prototype.record = function(key){
-    this.haveVotes[key]++;
-};
-
-Count.prototype.retrieve = function(key){
-    return this.haveVotes[key];
-};
-
-Count.prototype.results = function(key){
-    return this.haveVotes;
-};
-
-app.get('/', function(req, res){
-    res.render('index'){
-        title: "Puppies vs. Kittens"
-        votes: countVotes.results()
-    };
-});
-
 // Create a new Counter instance, like: `var counter = new Counter()`
 var countVotes = new Count();
 
-// Respond to 'get' requests for the route '/kittens'
-// - Record a vote for 'kittens'
-// - Retrieve the new cumulative votes for 'kittens'
-// - Respond with with the message:
-//     "Thank you for voting! Kittens have 12 total votes so far."
-
-app.post('/kittens', function(req, res){
-    countVotes.record('kittens');
-    res.send('Thank you for voting! Kittens have ' + countVotes.retrieve('kittens') + ' total votes so far.'); 
+app.get('/', function(req, res){
+    if(req.cookies.vote){
+        res.render('voted', {
+            title: "Puppies vs. Kittens",
+            votes: countVotes.results(),
+            animal: req.cookies.vote
+        });
+    } else {
+        res.render('notvoted', {
+            title: "Puppies vs. Kittens",
+            votes: countVotes.results()
+        });   
+    }
 });
 
-app.post('/kittens', function(req, res) {
-    res.cookies('voted', 'true',
-    secure: true,
-    maxAge: 1000*60*60*24*30 //2,592,000,000 sec?
-    )
-    res.render('vote', {
-        title: 'Welcome!'
+app.post('/vote', function(req, res){
+    countVotes.record(req.body.animal);
+    res.cookie('vote', req.body.animal, {
+        secure: true,
+        maxAge: 1000*60*60*24*30
     });
-});
-
-
-// Respond to 'get' requests for the route '/puppies'
-// - Record a vote for 'puppies'
-// - Retrieve the new cumulative votes for 'puppies'
-// - Respond with with the message:
-//     "Thank you for voting! Puppies have 12 total votes so far."
-
-app.post('/puppies', function(req, res){
-    countVotes.record('puppies');
-    res.send('Thank you for voting! Puppies have ' + countVotes.retrieve('puppies') + ' total votes so far.'); 
-});
-
-app.post('/puppies', function(req, res) {
-    res.cookies('voted', 'true',
-    secure: true,
-    maxAge: 1000*60*60*24*30 //2,592,000,000 sec?
-    )
-    res.render('vote', {
-        title: 'Welcome!'
-    });
+    res.redirect('/');
 });
 
 // Listen on port 8080 for Cloud9
