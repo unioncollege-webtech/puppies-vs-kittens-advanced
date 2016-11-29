035fdf1f4dec59611ec04769399f8cc2d8b6a030
anishb <bajagain.ansh@gmail.com>
2016-11-29 02:27:00 +0000

    Puppies vs kitten who is cutest?

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 // Require the Express module (https://npmjs.com/package/express)
-
+var express = require('express');
+var hbs = require('hbs');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var Count = require('./counter');
 
 // Create a new express application instance by calling `express()`
-
+var app = express();
 
 // Serve files in the 'public' directory with Express's built-in static file server
+app.use(express.static('public'));
+app.use(express.static('views'));
 
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(cookieParser());
 
 // Create a Counter class that will be used to create counter objects
 // See the full description in README.md
 // .
 
 // Create a new Counter instance, like: `var counter = new Counter()`
+var Scoreboard = new Count();
 
+app.get('/', function(req, res){
+    if(req.cookies.vote){
+        res.render('votes', {
+            title: "Puppies vs. Kittens",
+            score: Scoreboard.results(),
+            animal: req.cookies.vote
+        });
+    } else {
+        res.render('voting method', {
+            title: "Puppies vs. Kittens",
+            score: Scoreboard.results()
+        });   
+    }
+});
+
+app.post('/vote', function(req, res){
+    Scoreboard.record(req.body.animal);
+    res.cookie('vote', req.body.animal, {
+        secure: true,
+    });
+    res.redirect('/');
+});
 
 // Respond to 'get' requests for the route '/kittens'
 // - Record a vote for 'kittens'
@@ -29,3 +64,6 @@
 
 
 // Listen on port 8080 for Cloud9
+
+app.listen(8080);
+console.log("Server is up!");
